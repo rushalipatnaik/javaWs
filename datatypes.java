ff253e3ce32a29e6c5f238b17765489cf9073267
Rushali <rpatnaik78@gmail.com>
2022-01-17 16:08:59 +0530

    understanding datatypes

---
+++ datatypes.java
@@ -0,0 +1,20 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int a=10; 
+        short s=15;
+        byte b=11;
+        long l=117;
+        double i=6.5;
+        float f=18.3f;
+        char ch='n';
+        boolean bo = true;
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(ch);
+        System.out.println(bo);
+    }
+}
