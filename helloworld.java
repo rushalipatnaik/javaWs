5a1f9ebe21241aac642162bfa5113e465133d00e
Rushali <rpatnaik78@gmail.com>
2022-01-17 15:48:45 +0530

    minor changes

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,6 @@
 public class  helloworld {
-  
   public static void main(String args[]){
-      System.out.print("Hello World");
+      System.out.print("Hello World\n");
+      System.out.println(Heyyy);
   }
-
-
 }
