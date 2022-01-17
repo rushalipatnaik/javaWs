13b350675d2ccad282fd14e961f80a9d4eb8cd05
Rushali <rpatnaik78@gmail.com>
2022-01-17 15:55:24 +0530

    .

---
+++ addnumbers.java
@@ -0,0 +1,9 @@
+public class addnumbers {
+    
+    public static void main(String[] args) {
+        int a,b,c;
+        a=10; b=25; c=a+b;
+        System.out.println(c);
+    }
+    
+}
--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class  helloworld {
   public static void main(String args[]){
       System.out.print("Hello World\n");
-      System.out.println(Heyyy);
+      System.out.println("Heyyy");
   }
 }
